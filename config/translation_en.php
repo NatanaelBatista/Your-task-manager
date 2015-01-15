54e7c56311b93a64715e153b53c3fbd9b8cf7777
valdiney <valdiney.2@hotmail.com>
2015-01-15 08:33:31 -0200

    area de postar as tarefas traduzidas para o inglês

--- config/translation_en.php
+++ config/translation_en.php
@@ -29,3 +29,12 @@ $captions_menu_left["caption_menu_left_descricao_de_acao"] = "Click on the users
 /*Legendas do menu left. ( Lista de tarefas cadastradas )*/
 $captions_menu_left["caption_menu_left_tarefas"] = "Registering Tasks";
 /*end Lista de tarefas cadastradas*/
+
+/*======================================================================*/
+
+/*Legendas ( Postar tarefas )*/
+$captions_postar_tarefas["caption_input_titulo"] = "Write a title for the task...";
+$captions_postar_tarefas["caption_input_textarea"] = "Write a task...";
+$captions_postar_tarefas["caption_input_select"] = "Assign Task...";
+$captions_postar_tarefas["caption_button"] = "Publish";
+/*end Lista de tarefas cadastradas*/
--- layout_parts/area_textarea_postagem.php
+++ layout_parts/area_textarea_postagem.php
@@ -1,16 +1,16 @@
 <section class="areas area-text-post">
 	    <form method="post" action="tarefa_controller.php?cadastrar">
-	        <input type="text" name="titulo" id="titulo" placeholder="Titulo da terefa." value="<?php echo isset($_COOKIE["retornaTitulo"]) ? $_COOKIE["retornaTitulo"]:""; ?>">
+	        <input type="text" name="titulo" id="titulo" placeholder="<?php echo $captions_postar_tarefas["caption_input_titulo"]; ?>" value="<?php echo isset($_COOKIE["retornaTitulo"]) ? $_COOKIE["retornaTitulo"]:""; ?>">
 
-			<textarea name="texto" class="textarea-post" cols="2" rows="10" placeholder="Escreva uma terefa..."><?php echo isset($_COOKIE["retornoTexto"]) ? $_COOKIE["retornoTexto"]:""; ?></textarea>
+			<textarea name="texto" class="textarea-post" cols="2" rows="10" placeholder="<?php echo $captions_postar_tarefas["caption_input_textarea"]; ?>"><?php echo isset($_COOKIE["retornoTexto"]) ? $_COOKIE["retornoTexto"]:""; ?></textarea>
 			
 			<select name="tarefa_para_usuario" id="tarefa_para_usuario">
-				<option value="">Escolha a quem atribuir esta Tarefa...</option>
+				<option value=""><?php echo $captions_postar_tarefas["caption_input_select"]; ?></option>
 				<?php 
 				foreach($usuario->listar() as $listar):?>
                    <option value="<?php echo $listar->id;?>"><?php echo $listar->nome; ?></option>
 				<?php endforeach;?>
 			</select> <br>
-			<button type="submit" class="button-postar">Publicar</button>
+			<button type="submit" class="button-postar"><?php echo $captions_postar_tarefas["caption_button"]; ?></button>
 		</form>
 </section>		
